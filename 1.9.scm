0a0c35e9a2f6f42da40dfb445fc1816a081ca0fe
Wankai Zhang <wankaizhang@gmail.com>
2014-12-04 02:08:47 +0800

    Update 1.9.scm

--- 1.9.scm
+++ 1.9.scm
@@ -3,7 +3,7 @@
       b
       (inc (+ (dec a) b))))
 
-(+ 4 5)
+(+ 4 5) ; this is a recursive procedure
 (inc (+ 3 5))
 (inc (inc (+ 2 5)))
 (inc (inc (inc (+ 1 5))))
@@ -12,4 +12,21 @@
 (inc (inc (inc 6)))
 (inc (inc 7))
 (inc 8)
+9
+
+
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+
+(+ 4 5) ; this is a iterative procedure
+(+ (dec 4) (inc 5))
+(+ 3 6)
+(+ (dec 3) (inc 6))
+(+ 2 7)
+(+ (dec 2) (inc 7))
+(+ 1 8)
+(+ (dec 1) (inc 8))
+(+ 0 9)
 9
