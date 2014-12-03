a61d27e46abf996d3b081905354785614334aaff
Wankai Zhang <wankaizhang@gmail.com>
2014-12-04 02:02:13 +0800

    Update 1.8.scm

--- 1.8.scm
+++ 1.8.scm
@@ -1,5 +1,5 @@
 ; help function
-(define (square x) (* x x))
+(define (cube x) (* x x x))
 
 (define (cube-root x) (cube-root-iter 1.0 x))
 
@@ -7,3 +7,9 @@
   (if (good-enough? guess x)
       guess
       (cube-root-iter (improve guess x) x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) (* 0.001 x)))
+  
+(define (improve guess x)
+  (/ (+ (/ x (* guess guess)) (* 2 guess)) 3)
