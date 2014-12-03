fa2fdcc0964e7c54876de4e1604ec85c095a9501
Wankai Zhang <wankaizhang@gmail.com>
2014-12-04 01:52:06 +0800

    Update 1.7.scm

--- 1.7.scm
+++ 1.7.scm
@@ -1,1 +1,16 @@
+; help procedure
+(define (square x) (* x x))
+
 (define (sqrt x) (sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) (* 0.001 x)))
+
+(define (improve guess x)
+  (/ (+ guess (/ x guess)) 2))
+
