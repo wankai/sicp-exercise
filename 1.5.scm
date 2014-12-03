85d5155195c537f51e88cd19fb2f453cf09c9820
Wankai Zhang <wankaizhang@gmail.com>
2014-12-04 01:39:00 +0800

    Update 1.5.scm

--- 1.5.scm
+++ 1.5.scm
@@ -4,3 +4,16 @@
   (if (= x 0)
       0
       y))
+
+(test 0 (p))
+
+; applicative-order evaluation. loop forever
+(test 0 (p))
+(test 0 (p))
+(test 0 (p))
+....
+
+; normal-order evaluation. result is 0
+(test 0 (p))
+(if (= 0 0) 0 (p))
+0
