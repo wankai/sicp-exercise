ba2b2c11ba569df8239d6c2591352ac7327d98b4
Wankai Zhang <wankaizhang@gmail.com>
2014-12-04 21:30:23 +0800

    Create 1.12.scm

---
+++ 1.12.scm
@@ -0,0 +1,11 @@
+(define (pascal n)
+  (pascal n n))
+
+(define (display-pascal-iter row n)
+  (if (= row 0)
+      ()
+      ((display-pascal-line n n)
+       (display-pascal-iter (- n 1) n))))
+       
+(define (display-pascal-line row n)
+  (
