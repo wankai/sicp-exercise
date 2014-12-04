6e8e3db44d43c6d48fe9175bc31451fe80a6ab9c
Wankai Zhang <wankaizhang@gmail.com>
2014-12-04 19:58:51 +0800

    Update counting-change.scm

--- counting-change.scm
+++ counting-change.scm
@@ -4,4 +4,21 @@
 (define nickle 5)
 (define penny 1)
 
+(define (max-unit n)
+  (cond ((= n 5) half-dollar)
+        ((= n 4) quater)
+        ((= n 3) dime)
+        ((= n 2) nickle)
+        ((= n 1) pennu)))
+
 (define (counting-change money)
+  (counting-change money 5))
+  
+(define (counting-change-iter money n)
+  (cond ((< money 0) 0)
+        ((= money 0) 1)
+        ((= n 0) 0)
+        (else (if (ge money (max-unit n))
+                  (counting-change-iter (- money (max-unit n))
+                                        n)
+                  (counting-change-iter money (- n 1))))))
